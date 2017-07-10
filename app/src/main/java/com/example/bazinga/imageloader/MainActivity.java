0782fb17c74bd21c6e18ba98a09be80d17fed407
zhangyuwei <570146800@qq.com>
2017-07-10 19:30:54 +0800

    写完简单的图片加载累

--- app/src/main/java/com/example/bazinga/imageloader/MainActivity.java
+++ app/src/main/java/com/example/bazinga/imageloader/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.bazinga.imageloader;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView cahceImageView ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+
+    }
+
+    private void initView() {
+        cahceImageView = (ImageView)findViewById(R.id.cacheImage);
+        ImageLoader imageLoader = new ImageLoader();
+        imageLoader.displayImage("https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo/bd_logo1_31bdc765.png"
+                ,cahceImageView);
+
     }
 }
